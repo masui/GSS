7a99344508de416e96df68ef2ddfa688cb0499fb
Toshiyuki Masui <masui@pitecan.com>
2025-12-22 10:35:55 +0900

    ...

---
+++ settoken.js
@@ -0,0 +1,11 @@
+// @description  Set Gyazo Token
+// @author       Toshiyuki Masui
+// @match        https://scrapbox.io/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=example.com
+// @run-at       context-menu
+// @grant GM.setValue
+// ==/UserScript==
+
+//GM.setValue("GYAZO_TOKEN",token)
+
+// ~/Tampermonkey に移動
